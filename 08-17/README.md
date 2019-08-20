cb2474bdbbaffbb0e2555a603a47afcfd09feb1b
haoran <jianghaoran@outlook.com>
2019-08-20 13:27:59 +0800

    init vue project

---
+++ 08-17/README.md
@@ -0,0 +1,2 @@
+* npm install
+* npm run dev 启动前端
---
+++ 08-17/build/webpack.prod.js
@@ -0,0 +1,44 @@
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const commonConfig = require('./webpack.common.js');
+
+const prodConfig = {
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          'sass-loader',
+          'postcss-loader',
+        ],
+      }, {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].chunk.js',
+  },
+};
+
+module.exports = merge(commonConfig, prodConfig);
